7df6a1fa962ea0b1f6cd3f10711674d1ada9a6bc
pdenisenko <forthevk@gmail.com>
2021-01-10 18:32:03 +0200

    feat(api/di): add knex, add DB get posts list endpoint

---
+++ api/src/configs/db.config.js
@@ -0,0 +1,10 @@
+module.exports = require('knex')({
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  debug: true,
+});
