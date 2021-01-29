797efed99fae773e280270d08ca26ce084070349
pdenisenko <forthevk@gmail.com>
2021-01-29 18:29:12 +0200

    feat(front/router): add route date check for current

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, Redirect, useParams } from 'react-router-dom';
 
 import HomePage from 'containers/Main/HomePage';
 import ArticlePage from 'containers/Main/ArticlePage';
 import ArticleCreatePage from 'containers/Main/ArticlePage/Create';
 import ProfilePage from 'containers/Main/ProfilePage';
 import ProfileEditPage from 'containers/Main/ProfilePage/Edit';
+
+function ProfileDateCheck() {
+  const { date } = useParams();
+
+  const today = new Date();
+  const routeDate = new Date(date);
+
+  return routeDate >= today && <ProfilePage />;
+}
 
 function App() {
   return (
@@ -17,15 +26,18 @@ function App() {
         <Route path="/profile" exact component={ProfilePage} />
         <Route path="/profile/edit" exact component={ProfileEditPage} />
         <Route
-          path={[
-            '/users',
-            '/users/:id(\\d+)/(edit|avatar|file)?',
-            '/users/:id(\\d+)/avatar/(edit|delete)?',
-            '/users/:id(\\d+)/file/(\\d+)-([a-z]{1,10})-(\\d{4}-\\d{2}-\\d{2}).(docx|jpeg|pdf|txt)/v.(\\d{1}).(\\d{1}).(\\d{1})',
-          ]}
+          path={['/users', '/users/:id(\\d+)/(edit|avatar|file)?', '/users/:id(\\d+)/avatar/(edit|delete)?']}
           exact
           component={ProfilePage}
         />
+        <Route
+          path={
+            '/users/:id(\\d+)/file/(\\d+)-([a-z]{1,10})-:date(\\d{4}-\\d{2}-\\d{2}).(docx|jpeg|pdf|txt)/v.(\\d{1}).(\\d{1}).(\\d{1})'
+          }
+          exact
+        >
+          <ProfileDateCheck />
+        </Route>
         <Redirect to="/home" />
       </Switch>
     </BrowserRouter>
